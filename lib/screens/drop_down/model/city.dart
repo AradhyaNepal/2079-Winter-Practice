98961c828dcffd14a3e988edb558084e46644dc0
Aradhya Nepal <aradhya.1221@gmail.com>
2023-01-07 20:34:21 +0545

    Making Model

---
+++ lib/screens/drop_down/model/city.dart
@@ -0,0 +1,4 @@
+class City{
+  String name;
+  City({required this.name});
+}
---
+++ lib/screens/drop_down/model/district.dart
@@ -0,0 +1,12 @@
+import 'package:province_district_city_pick/screens/drop_down/model/city.dart';
+
+class District{
+  String name;
+  List<City> cityList;
+
+  District({
+    required this.name,
+    required this.cityList,
+  });
+
+}
