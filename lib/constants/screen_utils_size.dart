47888466459d5e4f068ee87711c5e7cdc18f7e30
Aradhya Nepal <aradhya.1221@gmail.com>
2023-01-07 20:11:31 +0545

    Deleting one

--- lib/constants/screen_utils_size.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class KScreenUtilsSize{
-  KScreenUtilsSize._();
-  static double height=500;
-  static double width=300;
-  static Size size=Size(width,height);
-}
