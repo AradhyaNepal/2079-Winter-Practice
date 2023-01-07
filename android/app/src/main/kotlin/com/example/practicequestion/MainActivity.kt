6dce8fd4959e2ef318a56dbea1524c1a85546ddd
Aradhya Nepal <aradhya.1221@gmail.com>
2023-01-07 19:30:38 +0545

    Clear

--- android/app/src/main/kotlin/com/example/practicequestion/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.practicequestion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/screen_size.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScreenSize{
-  ScreenSize._();
-  static const double kHeight=100;
-  static const double kWidth=100;
-  static const Size kScreenSize=Size(kWidth, kHeight);
-}
