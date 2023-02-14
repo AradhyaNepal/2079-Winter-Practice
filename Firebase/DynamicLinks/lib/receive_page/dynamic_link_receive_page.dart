74ee4b74b47cf6ab7d4c0ebf0de9f42b7b45d0a1
Aradhya Nepal <aradhya.1221@gmail.com>
2023-02-14 10:55:07 +0545

    Dynamic Link Completed, now testing

--- Firebase/DynamicLinks/lib/receive_page/dynamic_link_receive_page.dart
+++ Firebase/DynamicLinks/lib/receive_page/dynamic_link_receive_page.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_links/utils/dynamic_link_generator.dart';
 import 'package:flutter/material.dart';
 
 class DynamicLinkReceivePage extends StatelessWidget {
@@ -6,6 +7,32 @@ class DynamicLinkReceivePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final arguments = (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{}) as Map<String, dynamic>;
+    final size=MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height,
+      width: size.width,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dynamic Link Value Received"),
+          ),
+          body: Center(
+            child: Text(
+              _getText(arguments),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _getText(Map<String,dynamic> arguments){
+    final name=arguments[DynamicLinkGenerator.nameKey];
+    final phone=arguments[DynamicLinkGenerator.phoneKey];
+    String returnValue="";
+    returnValue+="Name: $name\n";
+    returnValue+="Phone: $phone.";
+    return returnValue;
   }
 }
--- Firebase/DynamicLinks/lib/utils/dynamic_link_initialize.dart
+++ Firebase/DynamicLinks/lib/utils/dynamic_link_initialize.dart
@@ -1,6 +1,8 @@
 import 'package:dynamic_links/firebase_options.dart';
 import 'package:dynamic_links/main.dart';
 import 'package:dynamic_links/receive_page/dynamic_link_receive_page.dart';
+import 'package:dynamic_links/send_page/dynamic_link_send_page.dart';
+import 'package:dynamic_links/utils/dynamic_link_generator.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/cupertino.dart';
@@ -20,10 +22,8 @@ class DynamicLinkInitializer{
     _listeningAlreadySubscribed=true;
     FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) async {
       try{
-        final String uri = Uri.decodeFull(dynamicLinkData.link.toString());
-        final values=uri.toString().split(".link/?").last.split("&");
-        MyApp.navigatorKey.currentState?.pushNamed(DynamicLinkReceivePage.route);
-
+        final paramsMap=dynamicLinkData.link.queryParameters;
+        MyApp.navigatorKey.currentState?.pushNamed(DynamicLinkReceivePage.route,arguments: paramsMap);
       }catch(e,s){
         debugPrint(e.toString());
         debugPrint(s.toString());
